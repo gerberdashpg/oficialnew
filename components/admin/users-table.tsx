465c01e2d68f73f4d315f55f8ecb3e9a15593c79
Gabriel Hipolito <gab230507@gmail.com>
2026-01-30 22:27:51 -0300

    1.2.1

--- components/admin/users-table.tsx
+++ components/admin/users-table.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/toast" // Import toast variable
+import { toast } from "@/components/ui/use-toast"
 import {
   Table,
   TableBody,
